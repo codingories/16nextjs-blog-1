a5102cf901ace404c2f3fd6f0d0eaa2faa4f7b15
朱骏杰 <ories.zhu@mochain.vip>
2020-07-11 20:32:40 +0800

    通过first-post.js演示CSS Modules

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,5 @@
 import React, {useCallback} from "react"
-import Link from "next/link"
-import Head from "next/dist/next-server/lib/head"
+import styles from 'styles/first-post.module.css'
 
 console.log('执行了2')
 export default function FirstPost(){
@@ -9,12 +8,12 @@ export default function FirstPost(){
   },[]) // useCallback 避免每次创建都执行
   return (
     <>
-      {/*React.Fragment*/}
       <div>
-        First Post
-        <button onClick={clickMe}>click me</button>
-        <hr/>
-        回到首页 <Link href="/"><a>点击这里</a></Link>
+        <div className={styles.wrapper}>
+          <div className={styles.content}>
+            内容
+          </div>
+        </div>
       </div>
     </>
   )
