56bd46af70fdd6878b1a5c131e9c521222ebbe6b
朱骏杰 <ories.zhu@mochain.vip>
2020-07-11 21:48:23 +0800

    通过配置next.config.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,10 @@
-import Head from 'next/head'
-import React from "react"
-import {s} from '../styles/s'
+import png from 'assets/images/1.jpg'
 export default function Index() {
   return (
     <div>
       <h1>标题1</h1>
       <p>段落</p>
-      <img src="/1.jpg" alt=""/>
-      {s}
-
-
+      <img src={png} alt=""/>
     </div>
   )
 }
