477faf9c594c48803e79111a27ae3cbb0a4af222
朱骏杰 <ories.zhu@mochain.vip>
2020-07-11 20:40:38 +0800

    将styled-jsx写法的文件分离

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,14 @@
 import Head from 'next/head'
 import React from "react"
-
+import {s} from '../styles/s'
 export default function Index() {
   return (
     <div>
       <h1>标题1</h1>
       <p>段落</p>
-      <style jsx>{`
-        h1{
-        color:red;
-        }
-      `}
-      </style>
-      <style jsx global>{`
-       body{
-        background-color:blue;
-       }
-`
-      }</style>
+      {s}
+
+
     </div>
   )
 }
---
+++ styles/s.js
@@ -0,0 +1,6 @@
+export const s = <style jsx="true">{`
+       body{
+        background-color:blue;
+       }
+`
+}</style>
