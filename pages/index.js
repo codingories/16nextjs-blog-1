4b88886987c2ef1a95292950a861b9227b62fadc
朱骏杰 <ories.zhu@mochain.vip>
2020-07-11 19:04:09 +0800

    配置Head，及React.Fragment演示

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>我的博客</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,6 @@
 import React, {useCallback} from "react"
 import Link from "next/link"
+import Head from "next/dist/next-server/lib/head"
 
 console.log('执行了2')
 export default function X(){
@@ -7,11 +8,18 @@ export default function X(){
     console.log('you click me')
   },[]) // useCallback 避免每次创建都执行
   return (
+    <>
+      {/*React.Fragment*/}
+      <Head>
+        <title>我的第一篇文章</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div>
         First Post
         <button onClick={clickMe}>click me</button>
         <hr/>
         回到首页 <Link href="/"><a>点击这里</a></Link>
       </div>
+    </>
   )
 }
