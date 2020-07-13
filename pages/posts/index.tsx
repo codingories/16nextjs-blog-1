2c9bfa43e73b454de18b40f5f3d39a8be246b419
朱骏杰 <ories.zhu@mochain.vip>
2020-07-13 22:42:57 +0800

    增加loading功能和增加请求结果空的时候的处理

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,6 +1,6 @@
-import {NextPage} from 'next';
-import axios from 'axios';
-import {useEffect, useState} from 'react';
+import {NextPage} from 'next'
+import axios from 'axios'
+import {useEffect, useState} from 'react'
 
 type Post = {
   id: string;
@@ -9,23 +9,34 @@ type Post = {
 }
 
 const PostsIndex: NextPage = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
+  const [posts, setPosts] = useState<Post[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isEmpty, setIsEmpty] = useState(false)
   useEffect(() => {
+    setIsLoading(true)
     axios.get('/api/v1/posts').then(response => {
-      console.log(response.data)
-      setPosts(response.data);
-    });
-  }, []); //写[]，表示第一次渲染请求，不写表示此次都请求
+      setTimeout(()=>{
+        setPosts(response.data)
+        setIsLoading(false)
+        if(response.data.length === 0){
+          setIsEmpty(true)
+        }
+      })
+    }), () => {
+      setIsLoading(false)
+    }
+  }, []) //写[]，表示第一次渲染请求，不写表示此次都请求
 
   return (
     <div>
-      {
-        posts.map(p=><div key={p.id}>{
+      <h1>文章列表</h1>
+      {isLoading ? <div>加载中</div> :
+        isEmpty ? <div>没有文章</div> :
+        posts.map(p => <div key={p.id}>{
           p.id
-        }</div>)
-      }
+        }</div>)}
     </div>
-  );
-};
+  )
+}
 
-export default PostsIndex;
+export default PostsIndex
